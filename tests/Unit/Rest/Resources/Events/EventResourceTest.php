05ca8dc2df297e3cbb27ae0c883f8499fce6f70f
Kieran Marshall <kieran.marshall@strawberry.co.uk>
2019-09-23 21:33:31 +0100

    wip

--- tests/Unit/Rest/Resources/Events/EventResourceTest.php
+++ tests/Unit/Rest/Resources/Events/EventResourceTest.php
@@ -16,4 +16,34 @@ final class EventResourceTest extends ResourceTestCase
 
     /** @var string */
     protected $dataPath = 'events/event';
+
+    public function testGet(): void
+    {
+        $this->queue(200, [], $this->response('get'));
+
+        $response = $this->resource->get();
+
+        $this->assertRequest('GET', 'events.json');
+        $this->assertCollection($response, 2);
+    }
+
+    public function testFind(): void
+    {
+        $this->queue(200, [], $this->response('find'));
+
+        $response = $this->resource->find(677313116);
+
+        $this->assertRequest('GET', 'events/677313116.json');
+        $this->assertModel($response);
+    }
+
+    public function testCount(): void
+    {
+        $this->queue(200, [], $this->response('count'));
+
+        $response = $this->resource->count();
+
+        $this->assertRequest('GET', 'events/count.json');
+        $this->assertSame(1, $response);
+    }
 }
--- tests/Unit/Rest/Resources/Events/WebhookResourceTest.php
+++ tests/Unit/Rest/Resources/Events/WebhookResourceTest.php
@@ -16,4 +16,69 @@ final class WebhookResourceTest extends ResourceTestCase
 
     /** @var string */
     protected $dataPath = 'events/webhook';
+
+    public function testGet(): void
+    {
+        $this->queue(200, [], $this->response('get'));
+
+        $response = $this->resource->get();
+
+        $this->assertRequest('GET', 'webhooks.json');
+        $this->assertCollection($response, 2);
+    }
+
+    public function testFind(): void
+    {
+        $this->queue(200, [], $this->response('find'));
+
+        $response = $this->resource->find(677313116);
+
+        $this->assertRequest('GET', 'webhooks/677313116.json');
+        $this->assertModel($response);
+    }
+
+    public function testCount(): void
+    {
+        $this->queue(200, [], $this->response('count'));
+
+        $response = $this->resource->count();
+
+        $this->assertRequest('GET', 'webhooks/count.json');
+        $this->assertSame(2, $response);
+    }
+
+    public function testCreate(): void
+    {
+        $this->queue(201, [], $this->response('create'));
+
+        $response = $this->resource->create(
+            $this->request('create')
+        );
+
+        $this->assertRequest('POST', 'webhooks.json');
+        $this->assertModel($response);
+    }
+
+    public function testUpdate(): void
+    {
+        $this->queue(200, [], $this->response('update'));
+
+        $response = $this->resource->update(
+            4759306,
+            $this->request('update')
+        );
+
+        $this->assertRequest('PUT', 'webhooks/4759306.json');
+        $this->assertModel($response);
+    }
+
+    public function testDelete(): void
+    {
+        $this->queue(200);
+
+        $response = $this->resource->delete(4759306);
+
+        $this->assertRequest('DELETE', 'webhooks/4759306.json');
+        $this->assertNull($response);
+    }
 }
